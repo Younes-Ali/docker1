3da3a29cf4499e1dacafa5bcec8e7eada242766c
Younes-Ali <younesaly.yoyo@gmail.com>
2025-08-30 23:21:34 +0300

    last virsion

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import socket # for network
+
 def sort_numbers(a, b, c):
     numbers = [a, b, c]
     numbers.sort()
@@ -11,3 +13,19 @@ if __name__ == "__main__":
 
     result = sort_numbers(x, y, z)
     print("Sorted numbers:", result)
+
+    # port part
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("localhost", 8080))  # make code open in port 8080
+    s.listen(1)
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        print("Connection from:", addr)
+
+        # for web
+        response_body = f"Sorted numbers: {sort_numbers(3, 1, 2)}"
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{response_body}"
+        client_socket.sendall(response.encode())
+        conn, addr = s.accept()    # accepting conection  
+        conn.close()               # closing conection
